c0a6c1d278af30f05998850875a57a344921f9fe
Leandro Lourenci <leandro@lourenci.com>
2017-11-09 20:00:58 -0200

    Install extract-text-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
 const path = require('path')
 
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
+
 const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js'
   },
+  plugins: [
+    new ExtractTextWebpackPlugin('style.bundle.css')
+  ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        loader: ExtractTextWebpackPlugin.extract({
+          use: 'css-loader'
+        })
       }
     ]
   }
